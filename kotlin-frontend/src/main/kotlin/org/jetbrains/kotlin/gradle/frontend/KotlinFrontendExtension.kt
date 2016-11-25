82bac7f346622e37547e1099cc5aecb77c90f317
Sergey Mashkov <sergey.mashkov@jetbrains.com>
2016-11-25 16:32:59 +0300

    Mark frontend extension properties as input

--- kotlin-frontend/src/main/kotlin/org/jetbrains/kotlin/gradle/frontend/KotlinFrontendExtension.kt
+++ kotlin-frontend/src/main/kotlin/org/jetbrains/kotlin/gradle/frontend/KotlinFrontendExtension.kt
@@ -1,6 +1,11 @@
 package org.jetbrains.kotlin.gradle.frontend
 
+import org.gradle.api.tasks.*
+
 open class KotlinFrontendExtension {
+    @Input
     var sourceMaps: Boolean = false
+
+    @Input
     var moduleName: String = ""
 }
