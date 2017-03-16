584656a13d40d894e3f034b404d55673a0365700
Sergey Mashkov <sergey.mashkov@jetbrains.com>
2017-03-16 20:21:01 +0300

    HMR kotlin library

---
+++ webpack-hmr-api/src/main/kotlin/org/webpack/hmr/HMR.kt
@@ -0,0 +1,19 @@
+package org.webpack.hmr
+
+external val module: Module
+
+external interface Module {
+    val hot: Hot?
+}
+
+external interface Hot {
+    val data: dynamic
+
+    fun accept()
+    fun accept(dependency: String, callback: () -> Unit)
+    fun accept(dependencies: Array<String>, callback: (updated: Array<String>) -> Unit)
+
+    fun dispose(callback: (data: dynamic) -> Unit)
+}
+
+external fun require(name: String): dynamic
