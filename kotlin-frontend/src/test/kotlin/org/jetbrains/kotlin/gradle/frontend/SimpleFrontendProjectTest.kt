59490f3691a6d04b6f7bd1c5d6faa969a3b06388
Sergey Mashkov <sergey.mashkov@jetbrains.com>
2017-11-24 12:30:49 +0300

    Upgrade Gradle to 4.3.1

--- kotlin-frontend/src/test/kotlin/org/jetbrains/kotlin/gradle/frontend/SimpleFrontendProjectTest.kt
+++ kotlin-frontend/src/test/kotlin/org/jetbrains/kotlin/gradle/frontend/SimpleFrontendProjectTest.kt
@@ -512,14 +512,15 @@ class SimpleFrontendProjectTest(gradleVersion: String, kotlinVersion: String) : 
         @JvmStatic
         @Parameters
         fun versions() = listOf(
-                arrayOf("3.1", "1.1.4-3"),
+//                arrayOf("3.1", "1.1.4-3"),
                 arrayOf("3.2.1", "1.1.4-3"),
                 arrayOf("3.3", "1.1.4-3"),
                 arrayOf("3.4.1", "1.1.4-3"),
                 arrayOf("3.5", "1.1.4-3"),
                 arrayOf("4.1", "1.1.4-3"),
                 arrayOf("4.2.1", "1.1.4-3"),
-                arrayOf("4.2.1", "1.1.51")
+                arrayOf("4.2.1", "1.1.51"),
+                arrayOf("4.3.1", "1.1.60")
         )
     }
 }
