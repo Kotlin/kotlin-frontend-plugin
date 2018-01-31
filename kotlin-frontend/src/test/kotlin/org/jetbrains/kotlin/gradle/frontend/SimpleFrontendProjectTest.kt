fa1f0e8f6befc5ec75fb0a9b1c67ecec52dc539d
Sergey Mashkov <sergey.mashkov@jetbrains.com>
2018-01-31 15:52:28 +0300

    Add integration test for Kotlin 1.2.21

--- kotlin-frontend/src/test/kotlin/org/jetbrains/kotlin/gradle/frontend/SimpleFrontendProjectTest.kt
+++ kotlin-frontend/src/test/kotlin/org/jetbrains/kotlin/gradle/frontend/SimpleFrontendProjectTest.kt
@@ -521,7 +521,8 @@ class SimpleFrontendProjectTest(gradleVersion: String, kotlinVersion: String) : 
                 arrayOf("4.2.1", "1.1.51"),
                 arrayOf("4.3.1", "1.1.60"),
                 arrayOf("4.3.1", "1.2.0"),
-                arrayOf("4.4", "1.2.0")
+                arrayOf("4.4", "1.2.0"),
+                arrayOf("4.4.1", "1.2.21")
         )
     }
 }
