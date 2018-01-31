fe4e87debbd69359687cc7986c5d333156e9fdb7
Sergey Mashkov <sergey.mashkov@jetbrains.com>
2018-01-31 15:26:52 +0300

    Fix double dependencies: don't depend on unpacked dependencies

--- kotlin-frontend/src/main/kotlin/org/jetbrains/kotlin/gradle/frontend/npm/NpmDependenciesTask.kt
+++ kotlin-frontend/src/main/kotlin/org/jetbrains/kotlin/gradle/frontend/npm/NpmDependenciesTask.kt
@@ -11,16 +11,36 @@ import java.util.*
 open class NpmDependenciesTask : DefaultTask() {
     var results: List<File> = emptyList()
 
+    @get:InputFile
+    val unpackedFiles: File by lazy { UnpackGradleDependenciesTask.unpackFile(project) }
+
     @TaskAction
     fun action() {
         val handled = HashSet<File>()
         project.tasks.withType(NpmIndexTask::class.java) { task ->
             if (handled.add(task.kotlinModulesList) && task.kotlinModulesList.canRead()) {
+                val imported = if (unpackedFiles.canRead()) {
+                    unpackedFiles.readLines()
+                            .map { it.trim()
+                                    .removeSuffix("/")
+                                    .removeSuffix(File.separator)
+                                    .substringAfterLast('/')
+                                    .substringAfterLast(File.separatorChar)
+                            }
+                            .distinct()
+                            .toSet()
+                } else emptySet()
+
                 val dirs = task.kotlinModulesList.readLines()
                         .map(String::trim)
                         .filter(String::isNotEmpty)
                         .map(::File)
                         .filter(File::exists)
+                        .filter {
+                            val parts = it.absolutePath.split(File.separator).dropWhile { it != "node_modules" }
+
+                            parts.size < 2 || parts[1] !in imported
+                        }
 
                 results = dirs
             }
