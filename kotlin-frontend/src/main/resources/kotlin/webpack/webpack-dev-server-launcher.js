972af11704a051c545a95ddab9614ff713807227
vkhikhlov <hihlow.vladislav@yandex.ru>
2018-02-14 02:15:53 +0300

    Support for passing options in devServer.

--- kotlin-frontend/src/main/resources/kotlin/webpack/webpack-dev-server-launcher.js
+++ kotlin-frontend/src/main/resources/kotlin/webpack/webpack-dev-server-launcher.js
@@ -12,18 +12,19 @@ var config = require(RunConfig.webPackConfig);
 
 for (var name in config.entry) {
     if (config.entry.hasOwnProperty(name)) {
-        config.entry[name] = [ "webpack-dev-server/client?http://" + RunConfig.host + ":" + RunConfig.port + "/", "webpack/hot/dev-server", config.entry[name] ];
+        config.entry[name] = [
+            "webpack-dev-server/client?http://" + RunConfig.host + ":" + RunConfig.port + "/",
+            "webpack/hot/dev-server",
+            config.entry[name]
+        ];
     }
 }
 
 if (!config.plugins) {
     config.plugins = [];
 }
-config.plugins.push(new webpack.HotModuleReplacementPlugin());
 
-if (!config.devServer) {
-    config.devServer = {};
-}
+config.plugins.push(new webpack.HotModuleReplacementPlugin());
 
 if (RunConfig.sourceMap) {
     config.module.rules.push({
@@ -34,37 +35,36 @@ if (RunConfig.sourceMap) {
     config.devtool = "eval-source-map";
 }
 
-config.devServer.inline = true;
-
-
-var devServer = new WebpackDevServer(
-    webpack(config),
-    {
-        publicPath: RunConfig.publicPath,
-        contentBase: (RunConfig.contentPath ? RunConfig.contentPath : undefined),
-        stats: RunConfig.stats || "errors-only",
-        host: RunConfig.host,
-        hot: true,
-        setup: function(app) {
-            app.get(RunConfig.shutDownPath, function(req, res) {
-                res.json({ shutdown: 'ok' });
-                devServer.close();
-            });
-        },
-        proxy: (RunConfig.proxyUrl) ? {
-            "**" : {
-                target: RunConfig.proxyUrl,
-                secure: false,
-                bypass: function(req, res, proxyOptions) {
-                    if (RunConfig.contentPath) {
-                        var file = path.join(RunConfig.contentPath, req.path);
-                        if (fs.existsSync(file)) {
-                            return req.path;
-                        }
+var devServerDefaultOptions = {
+    publicPath: RunConfig.publicPath,
+    contentBase: (RunConfig.contentPath ? RunConfig.contentPath : undefined),
+    stats: RunConfig.stats || "errors-only",
+    host: RunConfig.host,
+    hot: true,
+    inline: true,
+    setup: function (app) {
+        app.get(RunConfig.shutDownPath, function (req, res) {
+            res.json({shutdown: 'ok'});
+            devServer.close();
+        });
+    },
+    proxy: (RunConfig.proxyUrl) ? {
+        "**": {
+            target: RunConfig.proxyUrl,
+            secure: false,
+            bypass: function (req, res, proxyOptions) {
+                if (RunConfig.contentPath) {
+                    var file = path.join(RunConfig.contentPath, req.path);
+                    if (fs.existsSync(file)) {
+                        return req.path;
                     }
                 }
             }
-        } : undefined
-    }
-);
+        }
+    } : undefined
+};
+
+var compiler = webpack(config);
+var devServer = new WebpackDevServer(compiler, Object.assign(devServerDefaultOptions, config.devServer));
+
 devServer.listen(RunConfig.port, RunConfig.host);
